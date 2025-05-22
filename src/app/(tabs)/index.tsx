f227aaab7f333e2f582f66e60821248461e1b52d
Jeff <lmh.jeff@gmail.com>
2025-05-22 01:25:29 +0100

    test

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View className='flex-1 p-4'>
-      <Text className='text-white font-bold text-3xl'>Feed</Text>
+      <Text className='text-white font-bold text-3xl'>Feed!!!</Text>
     </View>
   );
 }
