8cf87ccf1df3ce3de680ddb693c13b3586cfd82a
Jeff <lmh.jeff@gmail.com>
2025-05-23 19:16:09 +0100

    create tanstack useMutation

--- src/app/(protected)/new.tsx
+++ src/app/(protected)/new.tsx
@@ -5,30 +5,48 @@ import {
   Pressable,
   KeyboardAvoidingView,
   Platform,
+  Alert,
 } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { supabase } from '@/lib/supabase';
 import { useAuth } from '@/providers/AuthProvider';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { router } from 'expo-router';
 
-const NewPostScreen = async () => {
+const createPost = async (content: string, user_id: string) => {
+  const { data } = await supabase
+    .from('posts')
+    .insert({
+      content,
+      user_id,
+    })
+    .select('*')
+    .throwOnError();
+
+  return data;
+};
+
+const NewPostScreen = () => {
   const [text, setText] = useState<string>('');
   const { user } = useAuth();
+  const queryClient = useQueryClient();
 
-  const onSubmit = async () => {
-    if (!text || !user) return;
-
-    const { data, error } = await supabase.from('posts').insert({
-      content: text,
-      user_id: user?.id,
-    });
-
-    if (error) {
+  const {
+    mutate: createPostMutation,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: () => createPost(text, user!.id ?? ''),
+    onSuccess: (data) => {
+      setText('');
+      router.back();
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
+    },
+    onError: (error) => {
       console.error(error);
-    }
-
-    setText('');
-  };
+    },
+  });
 
   return (
     <SafeAreaView edges={['bottom']} className='p-4 flex-1 h-80'>
@@ -38,6 +56,9 @@ const NewPostScreen = async () => {
         keyboardVerticalOffset={Platform.OS === 'ios' ? 140 : 0}
       >
         <Text className='text-white text-lg font-bold'>{'username'}</Text>
+
+        {error && <Text className='text-red-500 text-sm'>{error.message}</Text>}
+
         <TextInput
           value={text}
           onChangeText={setText}
@@ -50,7 +71,8 @@ const NewPostScreen = async () => {
 
         <View className='mt-auto'>
           <Pressable
-            onPress={onSubmit}
+            onPress={() => createPostMutation()}
+            disabled={isPending}
             className={`${
               text.length > 0 ? 'bg-white' : 'bg-gray-500'
             }  p-3 px-6 self-end rounded-full`}
